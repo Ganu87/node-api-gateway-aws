daa3ed8ff3f8bf40ceb4231b6c0c07850eff488a
ganesh shelke <ganesh.shelke@onsurity.com>
2025-06-18 12:07:07 +0530

    3 commit

--- product-service/app.js
+++ product-service/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 4003;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Product Service' });
+  res.json({ message: 'Hello from Product Service 1.1' });
 });
 
 app.listen(PORT, () => {
